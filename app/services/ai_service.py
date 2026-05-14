0891cbc4787ef0ead06049e5e7f1666d140dba27
Thay.min <134392865+thayzavi@users.noreply.github.com>
2026-05-14 22:16:50 +0000

    fix render

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,10 +1,26 @@
 from ultralytics import YOLO
+import os
 
-model = YOLO("model/dengue_model.pt")
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "model",
+    "dengue_model.pt"
+)
+
+model = YOLO(MODEL_PATH)
+
 
 def predict_image(image_path):
 
-    results = model(image_path)
+    results = model(
+        image_path,
+        imgsz=320,
+        verbose=False
+    )
 
     probs = results[0].probs
     names = results[0].names
