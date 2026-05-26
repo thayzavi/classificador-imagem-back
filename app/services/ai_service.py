51c360fe4d02614affdd9994af6c637599388ad6
Thay.min <134392865+thayzavi@users.noreply.github.com>
2026-05-26 18:47:01 +0000

    up_model_ai

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -11,10 +11,8 @@ BASE_DIR = os.path.dirname(
 MODEL_PATH = os.path.join(
     BASE_DIR,
     "model",
-    "modelo_dengue.tflite"
+    "best_float32.tflite"
 )
-
-
 
 interpreter = tf.lite.Interpreter(
     model_path=MODEL_PATH
@@ -22,15 +20,46 @@ interpreter = tf.lite.Interpreter(
 
 interpreter.allocate_tensors()
 
-
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 
+FOCOS_DENGUE = {
+
+    "positivo": {
+        "descricao": "Possível acúmulo de água parada identificado na imagem, podendo servir como criadouro do mosquito Aedes aegypti.",
+
+        "risco": "alto",
+
+        "prevencao": [
+            "Esvazie recipientes com água parada",
+            "Realize a limpeza do local",
+            "Tampe caixas d’água",
+            "Descarte pneus e objetos acumuladores de água",
+            "Mantenha calhas limpas"
+        ],
+
+        "orientacao": "Recomenda-se realizar a limpeza imediata do local."
+    },
+
+    "negativo": {
+        "descricao": "Nenhum possível foco de dengue foi identificado na imagem.",
+
+        "risco": "baixo",
+
+        "prevencao": [
+            "Continue monitorando o ambiente",
+            "Evite água parada",
+            "Mantenha recipientes fechados"
+        ],
+
+        "orientacao": "O ambiente aparenta estar seguro."
+    }
+}
+
 def preprocess_image(image_path):
 
     img = Image.open(image_path).convert("RGB")
-
     img = img.resize((224, 224))
 
     img_array = np.array(
@@ -38,7 +67,7 @@ def preprocess_image(image_path):
         dtype=np.float32
     )
 
-    img_array = (img_array / 127.5) - 1.0
+    img_array = img_array / 255.0
 
     img_array = np.expand_dims(
         img_array,
@@ -47,64 +76,40 @@ def preprocess_image(image_path):
 
     return img_array
 
-FOCOS_DENGUE = {
-    "positivo": {
-        "descricao": "Possível acúmulo de água parada identificado na imagem, podendo servir como criadouro do mosquito Aedes aegypti.",
-        "risco": "alto",
-        "prevencao": [
-            "Esvazie recipientes com água parada",
-            "Realize a limpeza do local",
-            "Tampe caixas d’água",
-            "Descarte pneus e objetos acumuladores de água",
-            "Mantenha calhas limpas"
-        ],
-        "orientacao": "Recomenda-se realizar a limpeza imediata do local e monitorar possíveis novos focos."
-    },
-
-    "negativo": {
-        "descricao": "Nenhum possível foco de dengue foi identificado na imagem.",
-        "risco": "baixo",
-        "prevencao": [
-            "Continue monitorando o ambiente",
-            "Evite acúmulo de água parada",
-            "Mantenha recipientes fechados"
-        ],
-        "orientacao": "O ambiente aparenta estar seguro, mas a prevenção deve continuar regularmente."
-    }
-}
-
-
 def predict_image(image_path):
 
     try:
 
         img_array = preprocess_image(image_path)
-
         interpreter.set_tensor(
             input_details[0]['index'],
             img_array
         )
 
         interpreter.invoke()
-
         prediction = interpreter.get_tensor(
             output_details[0]['index']
         )
 
-        confidence = float(prediction[0][0])
+        print("Prediction:", prediction)
 
-        # DEBUG
-        print("Prediction:", confidence)
+        negativo = float(prediction[0][0])
+        positivo = float(prediction[0][1])
 
-        # CASO POSITIVO
-        if confidence >= 0.75:
+
+        if positivo > negativo:
 
             info = FOCOS_DENGUE["positivo"]
 
             return {
+
                 "resultado": "Possível foco de dengue",
                 "classe": "positivo",
-                "confianca": round(confidence * 100, 2),
+
+                "confianca": round(
+                    positivo * 100,
+                    2
+                ),
 
                 "descricao": info["descricao"],
                 "risco": info["risco"],
@@ -112,13 +117,17 @@ def predict_image(image_path):
                 "orientacao": info["orientacao"]
             }
 
-        # CASO NEGATIVO
         info = FOCOS_DENGUE["negativo"]
 
         return {
+
             "resultado": "Sem foco de dengue",
             "classe": "negativo",
-            "confianca": round((1 - confidence) * 100, 2),
+
+            "confianca": round(
+                negativo * 100,
+                2
+            ),
 
             "descricao": info["descricao"],
             "risco": info["risco"],
@@ -129,6 +138,7 @@ def predict_image(image_path):
     except Exception as e:
 
         return {
+
             "resultado": "Erro ao analisar imagem",
             "classe": "erro",
             "confianca": 0,
