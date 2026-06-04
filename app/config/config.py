a5ef5fd843b7d1b823b9b1d4ff70f1f996b07376
Thay.min <134392865+thayzavi@users.noreply.github.com>
2026-06-04 20:14:10 +0000

    variaveis

--- app/config/config.py
+++ app/config/config.py
@@ -6,3 +6,6 @@ load_dotenv()
 class Config:
     MONGO_URI = os.getenv("MONGO_URI")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
